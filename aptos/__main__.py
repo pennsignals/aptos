a50c7969ac9bebe6dfe88cafdeb1c17f8bcf27bd
rightlag <rightlag@gmail.com>
2017-08-07 19:28:20 -0400

    Update main entry point arguments

--- aptos/__main__.py
+++ aptos/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import sys
 
 from .parser import SchemaParser
 from .visitor import ValidationVisitor
@@ -13,7 +14,7 @@ def validate(arguments):
     try:
         component.accept(ValidationVisitor(json.loads(arguments.instance)))
     except AssertionError as e:
-        print('Error: {}'.format(e))
+        sys.exit('Error: {}'.format(e))
 
 
 def convert(arguments):
@@ -27,17 +28,28 @@ def convert(arguments):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-schema', type=str, required=True)
-    subparsers = parser.add_subparsers()
+    parser = argparse.ArgumentParser(description='''
+        aptos is a tool for validating client-submitted data using the
+        JSON Schema vocabulary and converting JSON Schema documents to
+        different data-interchange formats.
+    ''', usage='%(prog)s [arguments] SCHEMA', epilog='''
+        More information on JSON Schema: http://json-schema.org/''')
+    subparsers = parser.add_subparsers(title='Arguments')
 
-    validation = subparsers.add_parser('validate')
-    validation.add_argument('-instance', type=str, default=json.dumps({}))
+    validation = subparsers.add_parser('validate',
+        help='Validate a JSON instance')
+    validation.add_argument('-instance', type=str, default=json.dumps({}),
+        help='JSON document being validated')
     validation.set_defaults(func=validate)
 
-    conversion = subparsers.add_parser('convert')
-    conversion.add_argument('-format', type=str)
+    conversion = subparsers.add_parser('convert',
+        help='Convert a JSON Schema to a data-interchange format')
+    conversion.add_argument('-format', type=str, choices=['avro'],
+        help='data-interchange format')
     conversion.set_defaults(func=convert)
+
+    parser.add_argument('schema', type=str,
+        help='JSON document containing the description')
 
     arguments = parser.parse_args()
     arguments.func(arguments)
