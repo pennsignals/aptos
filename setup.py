38105cb33c48ef7407d76facbcb843946dd486eb
rightlag <rightlag@gmail.com>
2017-08-08 09:24:51 -0400

    Update setup.py
    Include `url` and `author_email` keywords.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='aptos',
     version='1.0.0',
+    url='https://github.com/pennsignals/aptos',
     author='Jason Walsh',
+    author_email='jason.walsh@uphs.upenn.edu',
     maintainer='Jason Walsh',
     packages=find_packages(),
     tests_require=['nose'],
