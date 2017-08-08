c1cc2075cf2a0a2a735ac86e32f3debd432fdfb9
rightlag <rightlag@gmail.com>
2017-08-08 09:14:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aptos',
-    version='0.1.0',
+    version='1.0.0',
     author='Jason Walsh',
     maintainer='Jason Walsh',
     packages=find_packages(),
@@ -10,5 +10,6 @@ setup(
     test_suite='nose.collector',
     entry_points={
         'console_scripts': ['aptos = aptos.__main__:main'],
-    }
+    },
+    license='License :: OSI Approved :: Apache Software License'
 )
